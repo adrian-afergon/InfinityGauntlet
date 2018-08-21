73cf4db6303ce2eb4e64bdcdc91c16779ce2f343
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-08-21 21:54:15 +0100

    Make gauntlet stronger

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 const infinityGauntlet = {
-    powerStone: 'Controls all of the power in the universe. It can be used to augment or inhibit any force.',
-    usePowerStone: function() {
-        return 'Using super strength to crush the moon and throw it over to the Avengers!!';
+    powerStone: {
+        info: 'Controls all of the power in the universe. It can be used to augment or inhibit any force.',
+        equipped: false,
+        use () {
+            if(this.equipped) {
+                return 'Using super strength to crush the moon and throw it over to the Avengers!!';
+            } else {
+                return 'No Power Stone!!';
+            }
+
+        }
+    },
+    spaceStone: {
+        equipped: false,
+        info: 'Limitless manipulation of space, allowing for teleportation, dimensional manipulation, creation of wormholes, etc.',
+        use () {
+            if(this.equipped) {
+                return 'Create a wormhole and transport from planet Titan to Wakanda in a minute!';
+            } else {
+                return 'No Power Stone!!';
+            }
+
+        }
     }
 };
