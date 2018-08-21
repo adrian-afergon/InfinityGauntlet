b54fe738eaee51437696c42e3fbfdc333d4c911f
Adrián Ferrera González <adrian.afergon@gmail.com>
2018-08-21 21:48:54 +0100

    Give power to the stone

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 const infinityGauntlet = {
-    powerStone: 'Controls all of the power in the universe. It can be used to augment or inhibit any force.'
+    powerStone: 'Controls all of the power in the universe. It can be used to augment or inhibit any force.',
+    usePowerStone: function() {
+        return 'Using super strength to crush the moon and throw it over to the Avengers!!';
+    }
 };
